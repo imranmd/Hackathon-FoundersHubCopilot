d56dbd41d89abc553ed82bc4703e402fd110ca18
suag <suag@microsoft.com>
2024-09-17 21:24:09 +0530

    appbar and drawer

--- Frontend/founders-hub-app/src/App.js
+++ Frontend/founders-hub-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import MiniDrawer from './Drawer';
 
 function App() {
   return (
     <div className="app">
-      <div className="top-bar">
+      {/* <div className="top-bar">
         <div className="icon settings-icon">⚙️</div>
         <div className="icon profile-icon">👤</div>
       </div>
-      <h1 className="main-heading">Welcome to Founders Hub</h1>
+      <h1 className="main-heading">Welcome to Founders Hub</h1> */}
+      <MiniDrawer />
     </div>
   );
 }
