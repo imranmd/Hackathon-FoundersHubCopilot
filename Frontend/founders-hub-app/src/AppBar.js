d2d8a310fef45283e6c69aab62ccaf66e6b581ce
suag <suag@microsoft.com>
2024-09-17 21:39:04 +0530

    small fix

--- Frontend/founders-hub-app/src/AppBar.js
+++ Frontend/founders-hub-app/src/AppBar.js
@@ -6,7 +6,6 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
